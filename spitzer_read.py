3c8b33961ad83d018ffc9a4da09ba5576ac445dc
SigneMBarfred <118368226+SigneMBarfred@users.noreply.github.com>
2023-09-19 14:21:21 +0200

    code that reads multiple fits files from spitzer

---
+++ spitzer_read.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 12:32:44 2023
+
+@author: signe
+"""
+#input: path to specific folder containing choosen channel of spitzer data 
+#output: spitzer data stored in list
+
+from astropy.io import fits
+import os,glob
+
+
+folder_path = 'C:/Users/signe/Documents/DTU/Specialkursus_radiogal/irac_map_bcd_pbcd/r10789376/ch2/bcd'
+
+
+def spitzer_load(folder_path):
+    n = 0 
+    for filename in glob.glob(os.path.join(folder_path, '*.fits')):
+        hdu = fits.open(filename)
+        hdr = hdu[0].header  # the primary HDU header with metadata
+        data = hdu[0].data
+        #append coordinates to list? for all files in directoru, so as to make crossref w ragers easier? 
+        n = n+1
+        
+    print(n,'fits files found and read')
+    
+    if 'ch1' in folder_path:
+        wavelength = 3.6*10**-6
+        
+    elif 'ch2' in folder_path:
+        wavelength = 4.5*10**-6
+
+    elif 'ch3' in folder_path:
+         wavelength = 5.8*10**-6
+
+    elif 'ch4' in folder_path:
+        wavelength = 8.0*10**-6
+      
+    else: 
+        wavelength = 'unknown'
+
+    print('wavelength for measurements is', wavelength,'meters')
+
+    return data,hdr,n
+
+############ test ############
+
+hdr = spitzer_load(folder_path)[1]
+
+RA = hdr['RA_REF']
+DEC = hdr['DEC_REF']
+
