8e6d397674499ab89c0706d9f89b89b6151c0043
SigneMBarfred <118368226+SigneMBarfred@users.noreply.github.com>
2023-09-20 13:41:31 +0200

    Add files via upload

--- spitzer_read.py
+++ spitzer_read.py
@@ -8,7 +8,9 @@ Created on Tue Sep 19 12:32:44 2023
 #output: spitzer data stored in list
 
 from astropy.io import fits
+from astropy.wcs import WCS
 import os,glob
+from astropy.utils.data import get_pkg_data_filename
 
 
 folder_path = 'C:/Users/signe/Documents/DTU/Specialkursus_radiogal/irac_map_bcd_pbcd/r10789376/ch2/bcd'
@@ -20,8 +22,9 @@ def spitzer_load(folder_path):
         hdu = fits.open(filename)
         hdr = hdu[0].header  # the primary HDU header with metadata
         data = hdu[0].data
-        #append coordinates to list? for all files in directoru, so as to make crossref w ragers easier? 
+        #append coordinates to list? for all files in directory, so as to make crossref w ragers easier? 
         n = n+1
+        wcs = WCS(hdr)
         
     print(n,'fits files found and read')
     
@@ -42,11 +45,12 @@ def spitzer_load(folder_path):
 
     print('wavelength for measurements is', wavelength,'meters')
 
-    return data,hdr,n
+    return data,hdr,n,wcs
 
 ############ test ############
 
 hdr = spitzer_load(folder_path)[1]
+wcs = spitzer_load(folder_path)[3]
 
 RA = hdr['RA_REF']
 DEC = hdr['DEC_REF']
